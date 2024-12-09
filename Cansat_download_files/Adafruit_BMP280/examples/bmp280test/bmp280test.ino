35fe932b2168c619d5df1a972947278883640604
MoosMan06 <moosmw712@gmail.com>
2024-12-09 14:35:03 +0100

    Added BMP280 sensor

--- Cansat_download_files/Adafruit_BMP280/examples/bmp280test/bmp280test.ino
+++ Cansat_download_files/Adafruit_BMP280/examples/bmp280test/bmp280test.ino
@@ -22,12 +22,12 @@
 
 #define BMP_SCK 13
 #define BMP_MISO 12
-#define BMP_MOSI 11 
-#define BMP_CS 10
+#define BMP_MOSI 9
+#define BMP_CS 6
 
-Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
-//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 void setup() {
   Serial.begin(9600);
@@ -47,11 +47,7 @@ void loop() {
     Serial.print(F("Pressure = "));
     Serial.print(bmp.readPressure());
     Serial.println(" Pa");
-
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1013.25)); // this should be adjusted to your local forcase
-    Serial.println(" m");
     
     Serial.println();
-    delay(2000);
+    delay(1000);
 }
--- Satellite/Satellite.ino
+++ Satellite/Satellite.ino
@@ -1,20 +1,60 @@
-#include <SoftwareSerial.h>
+/*
+Arduino UNO wiring:
 
+  BMP280:
+    VCC <-> 3.3v
+    GND <-> GND
+SCK/SCL <-> 13  (required, SCK)
+SDA/SDI <-> 11  (required, MOSI)
+    CSB <-> 10  (~)
+    SDO <-> 12  (required, MISO)
+
+  APC220:
+    VCC <-> 5v
+    GND <-> GND
+     RX <-> 8
+     TX <-> 7
+*/
+
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 //apc220 config: 450000 2400 9 9600 0
 
-SoftwareSerial mySerial(10, 11);
-
-int number = 0;
+SoftwareSerial mySerial(8, 7);
 
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
+
+  Serial.println(F("BMP280 test"));
+  if (!bmp.begin()) {  
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
 }
 
+int Number;
+
 void loop() {
-  number++;
-  String message = "APC220 test " + String(number);
-  Serial.println(message);
-  mySerial.println(message);
-  delay(1000); //dies when set lower than 1s, idk why. Try increasing data transfer rate in setup
+  Number++;
+  String PacketNumber = String(Number); //string conversion magic is currently untested
+
+  float Temp = bmp.readTemperature(); // temperature in *C
+  int Pres = bmp.readPressure(); // pressure in Pa
+  
+  String packet = String(PacketNumber + "/" + Temp + "/" + Pres);
+  Serial.println(packet);
+  mySerial.println(packet);
+
+  delay(1000); //dies when set lower than 1s, idk why. Try increasing data transfer rate in apc220 setup
 }
