748050c58f0caa22c9b10338fa6832ff1ac38483
MoosMan06 <111775116+MoosMan06@users.noreply.github.com>
2025-02-03 22:32:32 +0100

    added apc220 config and renamed variables

--- Groundstation/Groundstation.ino
+++ Groundstation/Groundstation.ino
@@ -1,16 +1,18 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+// apc220 config: w 432600 1 9 0 0
+
+SoftwareSerial commsSerial(10, 11); // RX, TX
 
 void setup() {
   Serial.begin(9600);
-  mySerial.begin(9600);
+  commsSerial.begin(9600);
 }
 
 void loop() {
-  if (mySerial.available()) {
-    byte receivedByte = mySerial.read();
+  if (commsSerial.available()) {
+    byte receivedByte = commsSerial.read();
     Serial.write(receivedByte);
   }
 }
